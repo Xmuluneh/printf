b0ea552845cca771ec488aef044cba7cbeb64195
Xmuluneh <mulabr673@gmail.com>
2022-04-18 15:38:40 +0300

    Header file

--- main.h
+++ main.h
@@ -1,17 +1,11 @@
 #ifndef MAIN_H
-#define MAIN
+#define MAIN_H
 
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 
-/**
- * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
- *
- * Return: int
- */
+
 typedef struct structprint
 {
 	char *q;
